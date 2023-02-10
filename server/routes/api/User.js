4eae8f563b8033ed605720195993ac97bd54626e
Bondit Sinlee <bonditsinlee@gmail.com>
2023-02-09 23:49:35 -0600

    using express-validator to validate form input

--- server/routes/api/User.js
+++ server/routes/api/User.js
@@ -1,7 +1,24 @@
 const router = require('express').Router();
+const { body, validationResult } = require('express-validator');
 
-//Get api/users
+//Post api/users
+//Registering user
 
-router.get('/', (req,res) => res.send('User route'));
+router.post(
+    '/', 
+    [
+        body('name', 'Name is required').notEmpty(), // name must be filled
+        body('email', 'Email is required').isEmail(), // email can't be empty
+        
+        //password must be at least 5 chars long
+        body('password', 'Password is required and must be 5 char long').isLength({ min:5 }),
+    ],
+    (req,res) => {
+        const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    res.send(req.body);
+});
 
 module.exports = router;
