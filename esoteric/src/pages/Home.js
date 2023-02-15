c560e1bdac85b671ae3205ee51d4f1d8cab37d47
William Hirsch <Williamhirschh@gmail.com>
2023-02-14 19:18:33 -0600

    added code for home.js

--- esoteric/src/pages/Home.js
+++ esoteric/src/pages/Home.js
@@ -1,4 +1,16 @@
 import React from 'react';
 
-//change directory once those files are made
+
+
+
+
 import Auth from '../utils/auth';
+import { useQuery } from '@apollo/client';
+import { QUERY_THOUGHTS, QUERY_ME_BASIC } from '../utils/queries';
+//change directory once those files are made
+
+
+
+
+
+const loggedIn = Auth.loggedIn();
