8439db256336447f89fc2f2aec81a6d999d2ada4
William Hirsch <Williamhirschh@gmail.com>
2023-02-14 19:29:36 -0600

    added structure code for home.js'

--- esoteric/src/pages/Home.js
+++ esoteric/src/pages/Home.js
@@ -2,3 +2,19 @@ import React from 'react';
 
 //change directory once those files are made
 import Auth from '../utils/auth';
+import { useQuery } from '@apollo/client';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default Home;
