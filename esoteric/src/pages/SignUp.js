eb68bc1a67eb2b06eeeeb471c62dd72b50565546
Html-No0b <rmartin9797@gmail.com>
2023-02-14 20:46:12 -0600

    added signup

--- esoteric/src/pages/SignUp.js
+++ esoteric/src/pages/SignUp.js
@@ -53,4 +53,36 @@ const Signup = () => {
                   value={formState.username}
                   onChange={handleChange}
                 />
-               
+                <input
+                  className="form-input"
+                  placeholder="Your email"
+                  name="email"
+                  type="email"
+                  id="email"
+                  value={formState.email}
+                  onChange={handleChange}
+                />
+                <input
+                  className="form-input"
+                  placeholder="******"
+                  name="password"
+                  type="password"
+                  id="password"
+                  value={formState.password}
+                  onChange={handleChange}
+                />
+                <button className="btn d-block w-100" type="submit">
+                  Submit
+                </button>
+              </form>
+  
+              {error && <div>Signup failed</div>}
+            </div>
+          </div>
+        </div>
+      </main>
+    );
+  };
+  
+
+    export default Signup;
